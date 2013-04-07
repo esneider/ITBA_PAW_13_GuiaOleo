f7a5c7794a349bcf34ed74c4c5a35975d780e85e
lukkm <lmoscovicz@gmail.com>
2013-04-07 19:19:52 -0300

    Scripts bdd

--- TPE_2013/src/main/java/ar/edu/itba/it/paw/model/User.java
+++ TPE_2013/src/main/java/ar/edu/itba/it/paw/model/User.java
@@ -2,13 +2,13 @@ package ar.edu.itba.it.paw.model;
 
 public class User extends AbstractModel {
 
-	private String name, lastname, mail, username, password;
+	private String name, surname, mail, username, password;
 
-	public User(int id, String name, String lastname, String mail,
+	public User(int id, String name, String surname, String mail,
 			String username, String password) {
 		super(id);
 		this.name = name;
-		this.lastname = lastname;
+		this.surname = surname;
 		this.mail = mail;
 		this.username = username;
 		this.password = password;
