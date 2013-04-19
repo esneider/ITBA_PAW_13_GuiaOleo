4a05493402664845ed5d070f57842c99e8e1124d
lukkm <lmoscovicz@gmail.com>
2013-04-19 15:53:51 -0300

    deleted managers

--- TPE_2013/src/test/resources/update.sql
+++
@@ -1,3 +0,0 @@
-UPDATE foodtypes SET ammount = (SELECT COUNT(*) FROM restaurants WHERE restaurants.foodTypeId = foodtypes.id);
-UPDATE restaurants SET avgscore = (SELECT COALESCE(AVG(score), 0) FROM ratings WHERE ratings.restaurantId = restaurants.id);
-UPDATE restaurants SET cantRatings = (SELECT COUNT(*) FROM ratings WHERE ratings.restaurantId = restaurants.id);
