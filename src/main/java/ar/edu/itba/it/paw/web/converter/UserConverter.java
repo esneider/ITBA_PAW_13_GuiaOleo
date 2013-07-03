cfbf9a8add26e38d8a56cdbc21bdf8f128b82411
apomeran <alan_pomerantz@hotmail.com>
2013-07-03 20:42:35 -0300

    Added User Converter

--- src/main/java/ar/edu/itba/it/paw/web/converter/UserConverter.java
+++ src/main/java/ar/edu/itba/it/paw/web/converter/UserConverter.java
@@ -1,28 +1,35 @@
 package ar.edu.itba.it.paw.web.converter;
 
+import java.util.Locale;
+
+import org.apache.wicket.util.convert.IConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import ar.edu.itba.it.paw.domain.user.User;
 import ar.edu.itba.it.paw.domain.user.UserRepo;
 
-
+@SuppressWarnings("serial")
 @Component
-public class UserConverter implements Converter<String, User> {
+public class UserConverter implements IConverter<User> {
 
-    private UserRepo userRepo;
+	private UserRepo userRepo;
 
-    @Autowired
-    public UserConverter (UserRepo userRepo) {
+	@Autowired
+	public UserConverter(UserRepo userRepo) {
 
-        this.userRepo = userRepo;
-    }
+		this.userRepo = userRepo;
+	}
 
-    @Override
-    public User convert(String arg) {
+	@Override
+	public User convertToObject(String value, Locale arg1) {
+		return userRepo.get(Integer.valueOf(value));
 
-        return userRepo.get(Integer.valueOf(arg));
-    }
+	}
+
+	@Override
+	public String convertToString(User value, Locale arg1) {
+		return value.getUsername();
+	}
+
 }
-
